b69156f61e43dba0829d478786fa5dbb4fb86488
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-10 13:04:09 +0530

    second

--- patternprinting/hollowdiamaond.cpp
+++ patternprinting/hollowdiamaond.cpp
@@ -1,38 +1,38 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a number = ";
-//     cin>>n;
-//      for(int i=0;i<n;i++){
-//         for(int j=0;j<n-i-1;j++){
-//             cout<<"_";
-//         } 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number = ";
+    cin>>n;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<"_";
+        } 
        
-//         cout<<"*";
-//          if(i!=0){
-//        for(int k=0;k<2*i-1;k++){
-//           cout<<"_";
-//        }
-//        cout<<"*";
-//     }
-//        cout<<endl;
+        cout<<"*";
+         if(i!=0){
+       for(int k=0;k<2*i-1;k++){
+          cout<<"_";
+       }
+       cout<<"*";
+    }
+       cout<<endl;
    
-//       }
-//      for(int i=0;i<n-1;i++){
-//         for(int j=0;j<i+1;j++){
-//             cout<<"_";
-//         } 
+      }
+     for(int i=0;i<n-1;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"_";
+        } 
        
-//         cout<<"*";
-//         if(i!=n-2){
-//        for(int k=0;k<2*(n-i)-5;k++){
-//           cout<<"_";
-//        }
-//        cout<<"*";
-//     }
-//        cout<<endl;
+        cout<<"*";
+        if(i!=n-2){
+       for(int k=0;k<2*(n-i)-5;k++){
+          cout<<"_";
+       }
+       cout<<"*";
+    }
+       cout<<endl;
         
-//      }
-//     }
+     }
+    }
     
