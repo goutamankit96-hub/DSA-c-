c697ab3e2f9e58fe25a642610a7b1f8b87b88ebf
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-11 22:20:22 +0530

    fourth

--- patternprinting/tempCodeRunnerFile.cpp
+++ patternprinting/tempCodeRunnerFile.cpp
@@ -1,18 +1,13 @@
-#include<iostream>
-using namespace std;
-int main(){
+ #include<iostream>
+ using namespace std;
+ int main(){
     int n;
     cout<<"enter a number = ";
     cin>>n;
-     for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            cout<<" ";
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<"* ";
         }
-        for(int k=1;k<=i+1;k++){
-            cout<<k;
-        }
-        for(int p=i;p>0;p--){
-            cout<<p;
-        }cout<<endl;
+cout<<endl;
     }
-}
+ 
