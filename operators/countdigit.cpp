37075f963ba797481d64e9195b0d25087e62b337
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-18 21:20:46 +0530

    final

--- operators/countdigit.cpp
+++ operators/countdigit.cpp
@@ -1,15 +1,15 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a numbers : ";
-//     cin>>n;
-//     int digits=0;
-//     while(n>0){
-//         digits++;
-//         n=n/10;
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a numbers : ";
+    cin>>n;
+    int digits=0;
+    while(n>0){
+        digits++;
+        n=n/10;
         
-//     }
+    }
     
-//     cout<<digits<<endl;
-// }
+    cout<<digits<<endl;
+}
