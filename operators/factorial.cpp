8c70b6a00aee3a3832814a917e6a4d30e7f98d4c
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-18 21:23:57 +0530

    final

--- operators/factorial.cpp
+++ operators/factorial.cpp
@@ -1,13 +1,13 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a numbers : ";
-//     cin>>n;
-//     int fact=1;
-//     for(int i=1;i<=n;i++){
-//         fact=fact*1; 
-//         cout<<fact<<endl;
-//     }
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a numbers : ";
+    cin>>n;
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact=fact*i; 
+        cout<<fact<<endl;
+    }
    
-// }
+}
