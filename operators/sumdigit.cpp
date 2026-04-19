8ec0427fee014825428d57f11e404b177127e963
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-19 13:58:11 +0530

    final

--- operators/sumdigit.cpp
+++ operators/sumdigit.cpp
@@ -1,14 +1,14 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n;
-    cout<<"enter a numbers = ";
-    cin>>n;
-    int sum=0;
-    while(n>0){
-        int ld=n%10;
-        sum+=ld;
-        n=n/10;
-    }
-    cout<<sum<<endl;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter a numbers = ";
+//     cin>>n;
+//     int sum=0;
+//     while(n>0){
+//         int ld=n%10;
+//         sum+=ld;
+//         n=n/10;
+//     }
+//     cout<<sum<<endl;
+// // }
--- operators/sumofseries.cpp
+++ operators/sumofseries.cpp
@@ -1,17 +1,17 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter a numbers : ";
-//     cin>>n;
-//     int result=0;
-//     for(int i=1;i<=n;i++){
-//         if(n%2==0){
-//             result-=i;
-//         }
-//         else{
-//             result+=i;
-//         }
-//     }
-//     cout<<result<<endl;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a numbers : ";
+    cin>>n;
+    int result=0;
+    for(int i=1;i<=n;i++){
+        if(n%2==0){
+            result-=i;
+        }
+        else{
+            result+=i;
+        }
+    }
+    cout<<result<<endl;
+}
